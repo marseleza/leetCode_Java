b62b2e77c87254c2a1a789644d55b7e688804eee
marseleza <79719983+marseleza@users.noreply.github.com>
2024-04-23 14:37:27 +0300

    Update Solution.java

--- 2.AddTwoNumbers/Solution.java
+++ 2.AddTwoNumbers/Solution.java
@@ -1,7 +1,8 @@
 class Solution {
     static ListNode makeList(int num) {
-        if(num <= 0)
-            return new ListNode();
+        if(num == 0) {
+            return null;
+        }
         return new ListNode(num % 10, makeList(num / 10));
     }
     static int pow(int n1, int n2) {
@@ -14,12 +15,12 @@ class Solution {
         ListNode l2Save = l2;
 
         int l1Length = 0, l2Length = 0, l1Num = 0, l2Num = 0;
-        while(l1.next != null) {
+        while(l1 != null) {
             l1Length++;
             l1 = l1.next;
         }
 
-        while(l2.next != null) {
+        while(l2 != null) {
             l2Length++;
             l2 = l2.next;
         }
@@ -30,7 +31,7 @@ class Solution {
         int[] mas1 = new int[l1Length];
         int[] mas2 = new int[l2Length];
 
-        for (int i = 0; i < l1Length; i++) {
+        for (int i = 0; i < l1Length; i++) { //fill massive
             mas1[i] = l1.val;
             l1 = l1.next;
         }
@@ -40,7 +41,7 @@ class Solution {
             l2 = l2.next;
         }
 
-        for (int i = l1Length, k = 0; k < l1Length; i--, k++) {
+        for (int i = l1Length, k = 0; k < l1Length; i--, k++) { //make number from list
             l1Num = l1Num + pow(10, i - 1) * mas1[i - 1];
          }
 
@@ -48,19 +49,22 @@ class Solution {
             l2Num = l2Num + pow(10, i - 1) * mas2[i - 1];
         }
         int lSum = l1Num + l2Num;
+        if(lSum == 0) {
+            return new ListNode(0, null);
+        }
         return makeList(lSum);
     }
 
     public static void main(String[] args) {
-        ListNode num1 = makeList(243);
-        ListNode num2 = makeList(564);
+        ListNode num1 = makeList(0); //create two linked list
+        ListNode num2 = makeList(0);
 
         Solution test = new Solution();
 
         ListNode testMakeList;
         testMakeList = test.addTwoNumbers(num1, num2);
 
-        while (testMakeList.next != null) {
+        while (testMakeList != null) {
             System.out.print(testMakeList.val);
             testMakeList = testMakeList.next;
         }
